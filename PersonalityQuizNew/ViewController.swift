fe1b8b33b82f03ac1469de79d4a1d285950f460f
nunemel <nune.melikyan@yahoo.com>
2022-09-26 16:29:37 +0400

    Initial Commit

---
+++ PersonalityQuizNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityQuizNew
+//
+//  Created by Nune Melikyan on 26.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
